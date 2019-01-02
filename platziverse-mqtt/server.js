8c0ee4f0bcce8c125f6f1ffb262fe641bd933328
Ruben <ruben@MacBook-Pro-2017-15-Inch.local>
2019-01-02 16:01:53 -0300

    modulo 6 video 5 - Almacenamiento de informacion del agente y reporte de agente conectado

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -5,6 +5,9 @@ const mosca = require("mosca"); // para crear el servidor
 const redis = require("redis");
 const chalk = require("chalk"); // para tener colores en la terminal
 const db = require("platziverse-db");
+const configDB = require("platziverse-db/config.js");
+
+const { parsePayload } = require("./utils"); // somo me extrae la funcion parsePayload de la libreria utilidades
 
 const backend = {
   type: "redis",
@@ -18,14 +21,7 @@ const settings = {
   backend // informacion del backend
 };
 
-const config = {
-  database: process.env.DB_NAME || "platziverse",
-  username: process.env.DB_USER || "platzi",
-  password: process.env.DB_PASS || "platzi",
-  host: process.env.DB_HOST || "localhost",
-  dialect: "postgres",
-  logging: s => debug(s)
-};
+const config = configDB("platziverse:mqtt", false);
 
 // servicios que por ahora estaran indefinidos
 let Agent, Metric;
@@ -33,10 +29,22 @@ let Agent, Metric;
 // instanciamos el servidor
 const server = new mosca.Server(settings); // es un eventEmitter (agregamos funcionalidades cuando el servidor me lance eventos)
 
+// creamos una referencia de todos los agentes que tenemos conectados
+const clients = new Map();
+
+server.on("ready", async () => {
+  const services = await db(config).catch(handleFatalError);
+  Agent = services.Agent;
+  Metric = services.Metric;
+  // este evento es lanzado cuando el servidor este listo e inicializado
+  console.log(`${chalk.green("[platziverse-mqtt]")} server corriendo`);
+});
+
 // como recibiremos los distintos eventos a la red (cliente conectandose o desconectandose, publicando mensajes en el servidor, etc)
 server.on("clientConnected", client => {
   // evento de cliente que se conecta al servidor
   debug(`Cliente conectado con id: ${client.id}`); // id que autogenera mqtt
+  clients.set(client.id, null); // almaceno el cliente conectado al mapa
 });
 
 server.on("clientDisconnected", client => {
@@ -47,15 +55,45 @@ server.on("clientDisconnected", client => {
 server.on("published", (packet, client) => {
   // mensaje publicado dado un paquete y un cliente
   debug(`Recibido por el cliente de id: ${packet.topic}`); // topic es el tipo el mensaje (agentConnected, agentDisconnected o agentMessage)
-  debug(`Informacion que nos ha llegado: ${packet.payload}`); // payload es el contenido
-});
 
-server.on("ready", async () => {
-  const services = await db(config).catch(handleFatalError);
-  Agent = services.Agent;
-  Metric = services.Metric;
-  // este evento es lanzado cuando el servidor este listo e inicializado
-  console.log(`${chalk.green("[platziverse-mqtt]")} server corriendo`);
+  switch (packet.topic) {
+    case "agent/connected":
+    case "agent/disconnected":
+      debug(`Informacion que nos ha llegado: ${packet.payload}`); // payload es el contenido
+      break;
+    case "agent/message":
+      debug(`Informacion que nos ha llegado: ${packet.payload}`);
+      const payload = parsePayload(packet.payload);
+      if (payload) {
+        payload.agent.connected = true;
+        let agent;
+        try {
+          agent = await Agent.createOrUpdate(payload.agent);
+        } catch (e) {
+          return handleError(e); // salgo e la funcion y solo espero agentes con buena informacion, el resto los ignoro
+        }
+        debug(`Agente con id ${agent.uuid} guardado`);
+        // notificamos que el agente fue conectado
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent);
+          // le notificamos a todos nuestros clientes (hacemos un broucast) que estan conectados
+          // y escuchando en el evento de agente conectado que un agente se conecto
+          server.publish({
+            topic: "agent/connected",
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          });
+        }
+      }
+      break;
+  }
 });
 
 /*
@@ -69,6 +107,10 @@ function handleFatalError(err) {
   console.error(`${chalk.red("[fatal error]")} ${err.message}`);
   console.error(err.stack);
   process.exit(1); // tiramos el servidor cerrando el proceso
+}
+function handleError(err) {
+  console.error(`${chalk.red("[error]")} ${err.message}`);
+  console.error(err.stack); // no mata al proceso
 }
 
 // cuando tenemos un excepcion que no fue manejada (pasa a nivel del proceso) lo debemos manejar...
---
+++ platziverse-mqtt/utils.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function parsePayload (payload) {
+  // si el payload es un buffer...
+  if (payload instanceof Buffer) {
+    payload = payload.toString('utf8') // necesitamos que sea un string para hacer un jsonParse
+  }
+
+  // chequeamos por si las dudas si el payload no es un json y llega a ser otra estructura...
+  try {
+    payload = JSON.parse(payload)
+  } catch (e) {
+    payload = {}
+  }
+} // payload = contenido
+
+module.exports = {
+  parsePayload
+}
