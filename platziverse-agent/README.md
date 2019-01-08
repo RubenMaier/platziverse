f1c7df617e676f442b8cef1c6174c5daa98c2b88
RubenMaier <rmaierenzler@est.frba.utn.edu.ar>
2019-01-08 01:17:25 -0300

    Modulo 7 video 4 - Implemento metricas personalizadas

--- platziverse-agent/README.md
+++ platziverse-agent/README.md
@@ -7,23 +7,46 @@
 // como implementamos nuestro agente...
 const PlatziverseAgent = require('platziverse-agent') // yo obtengo platziverse-agent
 const agent = new PlatziverseAgent({
+    name: 'myApp',
+    username: 'admin',
     interval: 2000 // cada 2 milisegundo queremos que se emitan mensajes
 }) // creo una instancia
+
+// implementamos la api con:
+
+// funciones sincronas
+agent.addMetric('rss', function getRss() { // rss es la metrica, resident set sai
+    return process.memoryUsage().ress // lo obtenemos de process, en este caso me devuelve un json con los datos de memoria de este proceso
+})
+
+// funciones asincronas
+agent.addMetric('promiseMetric', function getRandomPromise() {
+    return Promise.resolve(Math.random())
+})
+
+// funciones asincronas con callback para transmitir valores
+agent.addMetric('callbackMetric', function getRandomCallaback (callback) {
+    setTimeout(() => {
+        callback(null, Math.random())
+    }, 1000)
+})
 
 agent.connect() // me conecto
 
 // estos eventos son unicamente de este agente
-agent.on('connected')
-agent.on('disconnected')
-agent.on('massage')
+agent.on('connected', handler) // el handler es el manejador
+agent.on('disconnected', handler)
+agent.on('message', handler)
 
 // estos eventos son de otros agentes que estan enviando a mi servidor mqtt
 // estos eventos los emito solo cuando mi uuid es distnto al de otro uuid (es decir, no son mis mensajes)
-agent.on('agent/connected')
-agent.on('agent/discconnected')
-agent.on('agent/message', payload => { // leo eventos de ese agente, ejemplo si recibo un agent message
+agent.on('agent/connected', handler)
+agent.on('agent/discconnected', handler)
+agent.on('agent/message', handler)
+
+function handler (payload) { // leo eventos de ese agente, ejemplo si recibo un agent message
     console.log(payload)
-})
+}
 
 setTimeout(() => agent.disconnect(), 20000) // despues de 20 segundo me desconecto
 
--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -6,6 +6,8 @@ const mqtt = require('mqtt') // es el cliente en este caso y no lo usamos como c
 const defaults = require('defaults')
 const { parsePayload } = require('./utils')
 const uuid = require('uuid')
+const os = require('os')
+const util = require('util')
 
 // configuro parametros por defecto
 const opcionesDefaults = {
@@ -26,6 +28,15 @@ class PlatziverseAgent extends EventEmitter {
     this._opciones = defaults(opciones, opcionesDefaults) // referencia interna de las opciones
     this._cliente = null
     this._agenteID = null
+    this._metricas = new Map()
+  }
+
+  addMetric(tipo, funcion) {
+    this._metricas.set(tipo, funcion)
+  }
+
+  removeMetric(type) {
+    this._metricas.delete(tipo)
   }
 
   connect() {
@@ -42,18 +53,42 @@ class PlatziverseAgent extends EventEmitter {
         this._started = true // luego de iniciar el timer, efectivamente marcamos que esto si inicializo
         this._agenteID = uuid.v4() // creamos un id unico para nuestro agente ayudandonos de una libreria llamada 'uuid'
         this.emit('connected', this._agenteID) // esto se transmite solo en este agente, no al servidor mqtt, ojo
-        this._timer = setInterval(() => { // creamos el intervalo
-          this.emit('agent/message', 'Esto es un mensaje que te va a poner a gozar') // emitimos un evento cada X cantidad de tiempo
+        this._timer = setInterval(async () => { // creamos el intervalo
+          if(this._metricas.size > 0) { // si tengo al menos una metrica...
+            let mensaje = {
+              agent: {
+                uuid: this._agenteID,
+                username: opciones.username,
+                name: opciones.name,
+                hostname: os.hostname() || 'localhost',
+                pid: process.pid
+              },
+              metrics: [], // enviamos las metricas como array vacio
+              timestamp: new Date().getTime()
+            }
+            for(let [ metrica, funcion] of this._metricas) { // iteramos todas las metricas que tenemos en el mapa de metricas (hacemos destructuring)
+              if(funcion.length == 1) { // si tiene un argumento es porque es callback
+                funcion = util.promisify(funcion) // convertimos funciones de callback a funciones de promesa (a partir de node 8)
+              }
+              mensaje.metrics.push({
+                type: metrica,
+                value: await Promise.resolve(funcion())
+              })
+            }
+            debug('Enviando', mensaje)
+            cliente.publish('agent/message', JSON.stringify(mensaje))
+            this.emit('mesagge', mensaje)
+          }
         }, opciones.interval) // El tiempo lo tomamos de las opciones que nos pasan
       })
-      cliente.on('massage', (topic, payload) => { // vemos si redistibuirlo o no
+      cliente.on('message', (topic, payload) => { // vemos si redistibuirlo o no
         payload = parsePayload(payload) // convertimos de string a JSON object
         // retransmito el mensaje, o no, dependiendo de ciertas condiciones
         let broadcast = false // por defecto no retransmito
         switch (topic) {
           case 'agent/connected':
           case 'agent/disconnected':
-          case 'agent/massage':
+          case 'agent/message':
             broadcast = payload && payload.agent && payload.agent.uuid != this._agenteID // asi me aseguro de no retransmitir mis propios mensajes
             break
         }
@@ -70,6 +105,7 @@ class PlatziverseAgent extends EventEmitter {
       clearInterval(this._timer) // paramos el intervalo con la referencia
       this._started = false // marcamos el estado como off
       this.emit('disconnect') // emitimos el evento de desconexion
+      this._cliente.end()
     }
   }
 }
